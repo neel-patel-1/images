e418ebe359549b3c778d2ba358cb1551423a2c11
Neel Patel <n869p538@york.ittc.ku.edu>
2023-05-27 06:26:16 -0500

    setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+QFLEX=$HOME/qflex-7-test
+git lfs fetch
+git lfs pull
+tar -xvf ubuntu16.tar.gz
+tar -xvf matmul.tar.gz
+mv ./matmul/* ./ubuntu16/
+$QFLEX/qemu/scripts/snap-manager.py --qemu-img-cmd-path $QFLEX/qemu update $QFLEX/images/ubuntu16/ubuntu.qcow2
